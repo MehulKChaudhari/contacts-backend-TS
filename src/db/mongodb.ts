df61174754caa83bcc4fd5e3d55731c4e5e860a7
mehulkchaudhari <chaudharimehul2001@gmail.com>
2022-02-03 06:47:12 +0530

    [ts-migrate][src] Init tsconfig.json file
    Co-authored-by: ts-migrate <>

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -1,4 +1,4 @@
-// @ts-expect-error ts-migrate(2451) FIXME: Cannot redeclare block-scoped variable 'mongoose'.
+//  FIXME: Cannot redeclare block-scoped variable 'mongoose'.
 const mongoose = require('mongoose')
 
 /**
